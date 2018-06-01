3f5851c506a420532fcba0538385a64af30d7c05
MissyM2 <missymaloney1@gmail.com>
2018-05-31 23:02:45 -0400

    0601commit

--- quizapp.js
+++ quizapp.js
@@ -1,34 +1,23 @@
 var state = {
-	qCounter: 1,
+	qCounter: 0,
 	cAns: 0,
-	incorrectAns: 0
-}
-
-function nextQuestion() {
-	console.log('made it to next question');
-	$('.qAndAContent').css('visibility','hidden');
-    //$('#js-quiz-form').hide();                
-	$("#submit-answer").hide();
-	$("#next-question").show();
+	incorrectAns: 0,
+	currentQ:""
+	
 }
 
 function correctAnswer() {
-	var cAnsContent = `<span id="correct answer">  Your answer is correct!  Yay!</span><br><br>`
-    $('.feedback').append(cAnsContent);
+    document.getElementById('answerdetail').innerHTML = 'Your answer is correct!  Yay';
     state.cAns +=1;
     document.getElementById('correct-answers').innerHTML = state.cAns + ' correct answers.  '; 
     document.getElementById('incorrect-answers').innerHTML = state.incorrectAns + ' incorrect answers'; 
-    nextQuestion();
 }
 
 function incorrectAnswer() {
-	console.log('made it to incorrectAnswer');
-	var iAnsContent = `<span id="incorrect answer">  Your answer is incorrect.  The correct answer is ${qContent[2].correctChoice}</span><br><br>`
-    $('.feedback').append(iAnsContent);
+	 document.getElementById('answerdetail').innerHTML = 'Your answer is incorrect!  The correct answer is ${qContent[state.qCounter].correctChoice};
     state.incorrectAns += 1;
     document.getElementById('correct-answers').innerHTML = state.cAns + ' correct answers.  '; 
-    document.getElementById('incorrect-answers').innerHTML = state.incorrectAns + ' incorrect answers'; 
-    nextQuestion();
+    document.getElementById('incorrect-answers').innerHTML = state.incorrectAns + ' incorrect answers';  
 }
 
 function userAnswer(){
@@ -37,42 +26,59 @@ function userAnswer(){
 		var selectedAnswer = $('input[name="answer"]:checked').parent().text();	
 		if (selectedAnswer) {
 			//check to see if the selectedAnswer matches qContent[for selected object in array].correctChoice
-			if (selectedAnswer == (qContent[2].correctChoice)) {
+			if (selectedAnswer == (qContent[state.qCounter].correctChoice)) {
 				correctAnswer();	
 			} else {
 				incorrectAnswer(); 
 			};
+			removePrevious();
+			setQuestion();
+			showQuestion();
 		} else {
 			$('#make-selection').show();
 		}
-        
     });
+}
+
+function removePrevious() {
+	$('.answerdetail').remove();
+	$('#whichq').remove();
+	$('.quiz-question').remove();
+    $('.quiz-answer-radiobuttons').remove();
+
+}
+
+function setQuestion() {
+	state.qCounter +=1; 
+	state.currentQ =  `<span class="quiz-question">${state.qCounter}    ${qContent[state.qCounter].q}</span><br><br>
+        <div class="quiz-answer-radiobuttons">
+          <label><input type="radio" name="answer" id="answer1" >${qContent[state.qCounter].choices[0]}</label><br>
+          <label><input type="radio" name="answer" id="answer2" >${qContent[state.qCounter].choices[1]}</label><br>
+          <label><input type="radio" name="answer" id="answer3" >${qContent[state.qCounter].choices[2]}</label><br>
+          <label><input type="radio" name="answer" id="answer4" >${qContent[state.qCounter].choices[3]}</label><br>
+        </div>` 
+
 }
 
 function showQuestion() {
 	//removes start button and shows form with first question.
-	var qAndAContent = `<span class="quiz-question">${qContent[2].q}</span><br><br>
-        <div class="quiz-answer-radiobuttons">
-          <label><input type="radio" name="answer" id="answer1" >${qContent[2].choices[0]}</label><br>
-          <label><input type="radio" name="answer" id="answer2" >${qContent[2].choices[1]}</label><br>
-          <label><input type="radio" name="answer" id="answer3" >${qContent[2].choices[2]}</label><br>
-          <label><input type="radio" name="answer" id="answer4" >${qContent[2].choices[3]}</label><br>
-        </div>`;
-     $('.qAndAContent').append(qAndAContent);
+	var whichq = `<span id="whichq">You are on question ${state.qCounter} out of ${qContent.length}.</span>`
+	$('#qnumber').append(whichq);
+     $('.qAndAContent').append(state.currentQ);
 }
 
 function startQuiz() {
 	//removes start button and shows form.
-	$(".start-quiz").click(function() {
+	$('.start-quiz').click(function() {
 	  $('.start-quiz').remove();
-	  $(".container").removeAttr("hidden");
+	  $('.container').removeAttr("hidden");
 	});
 }
 
 
 function quizApp() {
-	//console.log('made it to quizApp');
 	startQuiz(); //removes start button and shows form.
+	setQuestion(); //set var currentQ to proper information.
 	showQuestion(); //shows the first question.
 	userAnswer();
 }
